90d3e16bccb9b8740ada6370d1013dca479e1fce
Sebastian Tkalčić <stkalcic00@gmail.com>
2023-06-28 12:11:42 +0200

    edited App.js to include loggedIn and setLoggedIn state,

--- sports-calendar/src/App.js
+++ sports-calendar/src/App.js
@@ -1,38 +1,43 @@
-import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./Components/HomePage/HomePage";
-import CityDisplay from "./Components/City/CityMain";
-import RootLayout from "./Components/Root/Root";
-import CountyDisplay from "./Components/County/CountyMain";
-import LocationDisplay from "./Components/Location/LocationMain";
-import "bootstrap/dist/css/bootstrap.min.css";
-import CityPut from "./Components/City/CityPut";
-import UpdateCounty from "./Components/County/CountyUpdate";
-import UpdateLocation from "./Components/Location/UpdateLocation";
-
-import Calendar from "./Components/Event/Calendar";
-import Event from "./Components/Event/Event";
-import Home from "./Components/HomePage/Home";
-
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <RootLayout />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "County", element: <CountyDisplay /> },
-      { path: "Location", element: <LocationDisplay /> },
-      { path: 'City', element: < CityDisplay /> },
-      { path: "update-city/:id", element: <CityPut /> },
-      { path: "update-county/:id", element: <UpdateCounty /> },
-      { path: "update-location/:id", element: <UpdateLocation /> },
-      { path: "/Home", element: <Home /> },
-      { path: "/Event/:eventId", element: <Event /> },
-    ],
-  },
-]);
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './Components/HomePage/Home';
+import CityDisplay from './Components/City/CityMain';
+import RootLayout from './Components/Root/Root';
+import CountyDisplay from './Components/County/CountyMain';
+import LocationDisplay from './Components/Location/LocationMain';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CityPut from './Components/City/CityPut';
+import UpdateCounty from './Components/County/CountyUpdate';
+import UpdateLocation from './Components/Location/UpdateLocation';
+import Login from './Components/Auth/Login';
+import UserRegister from './Components/Register/UserRegister';
+import { useState } from 'react';
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.token ? true : false
+  );
+
+  const router = createBrowserRouter([
+    {
+      path: '',
+      element: <RootLayout loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>,
+      children: [
+        { path: '', element: <Home /> },
+        { path: '/', element: <Home /> },
+        { path: '/login', element: <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>},
+        { path: '/register', element: <UserRegister />},
+        { path: 'City', element: <CityDisplay /> },
+        { path: 'County', element: <CountyDisplay /> },
+        { path: 'Location', element: <LocationDisplay /> },
+        { path: 'update-city/:id', element: <CityPut /> },
+        { path: 'update-county/:id', element: <UpdateCounty /> },
+        { path: 'update-location/:id', element: <UpdateLocation /> },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
