fe79850fe88c8e71e42a4f1fda1a3295b73e90cd
DinoKnezevic1 <knezevic7p@gmail.com>
2023-06-30 10:34:13 +0200

    Admin options display based on user status

--- sports-calendar/src/Components/MainNavigation/NavBar.jsx
+++ sports-calendar/src/Components/MainNavigation/NavBar.jsx
@@ -58,49 +58,46 @@ function Example(props) {
                 <DropdownItem>account manager</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Admin options
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem tag={Link} to="/City">
-                  Go to City
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/County">
-                  Go To County
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/Location">
-                  Go To Location
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/Review">
-                  Go To Review
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/Placement">
-                  Placements
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/Sponsor">
-                  Sponsors
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/EventPost">
-                Event Post
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/Users">
-                  Go To Users
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem tag={Link} to="/EventSponsor">
-                  Event Sponsor
-                </DropdownItem>
-                <DropdownItem divider />
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            { props.loggedIn && props.role === "Super_admin" ? (
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Admin options
+                </DropdownToggle>
+                <DropdownMenu end>
+                  <DropdownItem tag={Link} to="/City">
+                    Go to City
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/County">
+                    Go To County
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/Location">
+                    Go To Location
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/Review">
+                    Go To Review
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/Placement">
+                    Placements
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/Sponsor">
+                    Sponsors
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/EventPost">
+                    Event Post
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/Users">
+                    Go To Users
+                  </DropdownItem>
+                  <DropdownItem divider />
+                </DropdownMenu>
+              </UncontrolledDropdown> ) : null}
           </Nav>
           <Nav className="navbar-nav ml-auto disable-flex-grow" navbar>
             <NavItem>
--- sports-calendar/src/Components/Root/Root.jsx
+++ sports-calendar/src/Components/Root/Root.jsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../MainNavigation/NavBar";
 import PlacementDisplay from "../Placement/PlacementMain";
 
-function RootLayout(props){
-  
-  return <>
-     <NavBar setLoggedIn={ props.setLoggedIn } loggedIn={ props.loggedIn }/>
-     <Outlet />
-        </>;
-};
+function RootLayout(props) {
+  return (
+    <>
+      <NavBar
+        role={props.role}
+        setLoggedIn={props.setLoggedIn}
+        loggedIn={props.loggedIn}
+      />
+      <Outlet />
+    </>
+  );
+}
 
 export default RootLayout;
